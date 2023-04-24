b768354ebf2a51e382b489847966ad5264910a18
Lekan <jimoridwanlekan@gmail.com>
2023-04-24 22:45:28 +0100

    lint project

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -13,20 +13,24 @@ const Calculator = () => {
 
   useEffect(() => {
     if (bill > 0 && tip > 0 && people !== null && people > 0) {
-      const tipAmount = ((bill * tip) / 100) / people;
-      const totalAmount = (bill / people) + tipAmount;
+      const tipAmount = (bill * tip) / 100 / people;
+      const totalAmount = bill / people + tipAmount;
       setTipAmount(parseFloat(tipAmount.toFixed(2)));
       setTotal(parseFloat(totalAmount.toFixed(2)));
     }
   }, [bill, tip, people]);
 
   return (
-    <div className="w-1/2 h-96 rounded-2xl border border-solid border-white bg-White py-8 px-6 shadow">
+    <div className="h-96 w-1/2 rounded-2xl border border-solid border-white bg-White px-6 py-8 shadow">
       <div className="flex gap-8">
-        <div className="flex flex-col gap-8 w-full">
+        <div className="flex w-full flex-col gap-8">
           <Input label="Bill" classname="bill-container" setBill={setBill} />
-          <Percentage setTip={setTip}  />
-          <Input label="Number of People" classname="people-container" setPeople={setPeople} />
+          <Percentage setTip={setTip} />
+          <Input
+            label="Number of People"
+            classname="people-container"
+            setPeople={setPeople}
+          />
         </div>
         <Amounts tipAmount={tipAmount} total={total} />
       </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,11 +7,16 @@ type InputProps = {
   setPeople?: (people: number) => void;
 };
 
-const Input: React.FC<InputProps> = ({ label, classname, setBill, setPeople }) => {
+const Input: React.FC<InputProps> = ({
+  label,
+  classname,
+  setBill,
+  setPeople,
+}) => {
   return (
     <div className={`${classname}`}>
       <label
-        className="mb-2 block text-sm font-bold text-grayishCyan font-mono"
+        className="mb-2 block font-mono text-sm font-bold text-grayishCyan"
         htmlFor="bill"
       >
         {label}
@@ -19,7 +24,7 @@ const Input: React.FC<InputProps> = ({ label, classname, setBill, setPeople }) =
       <input
         type="text"
         placeholder="0"
-        className="block text-xl h-9 font-mono w-64 appearance-none rounded border-gray-200 bg-gray-200 px-4 py-3 text-right leading-tight text-veryDarkCyan focus:border-gray-500 focus:outline-none"
+        className="block h-9 w-64 appearance-none rounded border-gray-200 bg-gray-200 px-4 py-3 text-right font-mono text-xl leading-tight text-veryDarkCyan focus:border-gray-500 focus:outline-none"
         onChange={(e) => {
           if (setBill) {
             setBill(e.target.valueAsNumber);
